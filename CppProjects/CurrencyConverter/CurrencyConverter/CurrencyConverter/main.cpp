1882fd40960c77b411ebe80df12a0ede6d4f08d9
Nolan Cauley <nolancauley88@gmail.com>
2021-05-27 23:12:11 -0400

    CurrencyConverter init

---
+++ CppProjects/CurrencyConverter/CurrencyConverter/CurrencyConverter/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  CurrencyConverter
+//
+//  Created by Nolan M.V. Cauley on 5/27/21.
+//
+#include "main.h"
+
+string response;
+Converter con;
+
+int main() {
+    
+    con.displayChoices();
+
+    cout<< "Type the currency you are inserting" << endl;
+//    cin >> response;
+    return 0;
+}
+
---
+++ CppProjects/CurrencyConverter/CurrencyConverter/CurrencyConverter/main.h
@@ -0,0 +1,18 @@
+//
+//  main.h
+//  CurrencyConverter
+//
+//  Created by Nolan M.V. Cauley on 5/27/21.
+//
+
+#ifndef main_h
+#define main_h
+
+#include <iostream>
+#include <string>
+#include "Converter.cpp"
+
+using namespace std;
+
+
+#endif /* main_h */
