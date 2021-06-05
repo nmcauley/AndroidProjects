78d80f32f788a50000caa2dd663d322c5b5fb357
Nolan Cauley <nolancauley88@gmail.com>
2021-06-05 15:38:32 -0400

    added client and server functionality

---
+++ PyProjects/SocketExercise/client.py
@@ -0,0 +1,29 @@
+import socket
+
+HEADER = 64
+# port and server
+PORT = 5050
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    # msg = input("Type your msg!")
+    # while msg != "esc":
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+
+    # we need to pad to our designated 64 length
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+
+send("HI")
+send(DISCONNECT_MSG)
--- PyProjects/SocketExercise/server.py
+++ PyProjects/SocketExercise/server.py
@@ -1,5 +1,47 @@
 import socket
 import threading
-import time
 
+HEADER = 64
+# port and server
 PORT = 5050
+# obtain IP address
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!DISCONNECT"
+
+# bind socket to that address
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
+# input -> connection, address
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected")
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        # checks if msg is not empty
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MSG:
+                connected = False
+
+            print(f"[{addr}] {msg}")
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
+print("[STARTING] server is now starting...")
+start()
